3b8cc7a581077b00e1ddf25c4186d8f211acc94f
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-11-27 14:25:35 +0300

    create FormUserDetails

--- src/Components/FormUserDetails.js
+++ src/Components/FormUserDetails.js
@@ -2,11 +2,17 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Icon, TextField } from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
+const styles = {
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    padding: 10
+}
 
 class FormUserDetails extends React.Component {
 
@@ -21,12 +27,48 @@ class FormUserDetails extends React.Component {
     
 
     return (
-        <AppBar style={{ backgroundColor:'teal' }}>
+      <ThemeProvider>
+        <AppBar style={{ backgroundColor: 'teal' }}>
           <Toolbar>
             <MenuIcon > </MenuIcon>
             <Typography style={{ margin: "auto" }} variant="h6"> Enter User Details </Typography>
-          </Toolbar>      
+          </Toolbar>
         </AppBar>
+        <br></br><br></br><br></br><br></br>
+        <div style={styles}>
+          <TextField
+            id="standard-basic"
+            label="First Name"
+            variant="outlined"
+          />
+        </div>
+        <div style={styles}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Last Name"
+            variant="outlined"
+          />
+        </div>
+        <div style={styles}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Email"
+            variant="outlined"
+          />
+        </div>
+        <div style={styles}>
+          <Button 
+            variant="contained" 
+            style={{ backgroundColor: "teal", color: "#fff", marginTop: 50 }}
+            onClick={this.continue}
+            >Continue
+        </Button>
+        </div>
+      </ThemeProvider>
+      
+
+
+        
       
     );
   }
